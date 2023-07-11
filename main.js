533fa733939cc1017621415d30e231eb54e329cf
mmednik <mmednik@gmail.com>
2023-07-11 16:20:34 -0300

    torus geometry

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight);
-var renderer = new THREE.WebGLRenderer({antialias: true});
+var camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight
+);
+var renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry(1,1,1);
-var material = new THREE.MeshBasicMaterial({color:0xff0000});
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+var geometry = new THREE.TorusGeometry(8, 1, 5, 30);
+var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var element = new THREE.Mesh(geometry, material);
+scene.add(element);
 
-cube.position.z = -5;
+element.position.z = -25;
 
 let animate = () => {
-  cube.rotation.x += 0.01;
+  element.rotation.x += 0.01;
+  camera.position.z += 0.01;
   renderer.render(scene, camera);
   requestAnimationFrame(animate);
-}
+};
 
 animate();
