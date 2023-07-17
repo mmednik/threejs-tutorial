ca96978a7ade87641b6202cdf1a5ff35a7d51cc7
mmednik <mmednik@gmail.com>
2023-07-17 16:17:15 -0300

    fix glb path

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from "@react-three/drei";
 
 export default function Home() {
-  const gltf = useLoader(GLTFLoader, './frog.glb');
+  const gltf = useLoader(GLTFLoader, "frog.glb");
   return (
     <div className="h-screen">
       <Canvas camera={{position: [0, 0, -10]}}>
